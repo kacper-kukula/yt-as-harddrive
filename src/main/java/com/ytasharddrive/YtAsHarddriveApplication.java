68d7dcdeee9d34f67fbf03731a4308b4a46480e2
Kacper <kacperkukula97@gmail.com>
2024-06-16 15:20:00 +0200

    Initial commit

---
+++ src/main/java/com/ytasharddrive/YtAsHarddriveApplication.java
@@ -0,0 +1,13 @@
+package com.ytasharddrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtAsHarddriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtAsHarddriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ytasharddrive/YtAsHarddriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytasharddrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtAsHarddriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
