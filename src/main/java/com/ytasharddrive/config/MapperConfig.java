39473d848d9c2338f45f52bcff9e2586ebe91f1b
Kacper <kacperkukula97@gmail.com>
2025-01-13 20:39:48 +0100

    Initial project setup with Spring Boot, MongoDB and checkstyle ruleset, and other dependencies

---
+++ src/main/java/com/ytasharddrive/config/MapperConfig.java
@@ -0,0 +1,13 @@
+package com.ytasharddrive.config;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.NullValueCheckStrategy;
+
+@org.mapstruct.MapperConfig(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        implementationPackage = "<PACKAGE_NAME>.impl"
+)
+public class MapperConfig {
+}
